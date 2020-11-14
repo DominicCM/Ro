9b20f2aadbf7b94b25c707e939d982879b69659d
Nathan <71985262+NathanCubing@users.noreply.github.com>
2020-11-14 11:57:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -16,42 +16,36 @@ client.on('message', (message) => {
 client.on('message', async message => {
     if(message.content === "?ping"){
         message.channel.send(`Pong! Latency: ${Date.now() - message.createdTimestamp} ms`);
-    } else if {
-        if(message.content === "?help"){
-            const embed = new Discord.MessageEmbed()
-            .setTitle("**Help Menu**")
-            .setDescription("**The bots prefix is: ?***")
-            .addField("Commands", "`ping` `whoiscool` `whatareyou` `pingdominic` `pingmortal` `pingpotato` `banurself` `kickurself` `kick` `ban` `info`")
-            .setFooter("Made by DominicCM, NathIsEmortal, Deni")
-            .setTimestamp()
-            message.channel.send(embed)
-        } else if {
-            if(message.content === "?whoiscoool"){
-                message.channel.send("Is cool (JK emortal is)")
-            } else if {
-                if(message.content === "?whatareyou"){
-                    message.channel.send("I am a bot. https://www.quora.com/What-is-a-discord-bot-What-is-a-discord-server")
-                } else if {
-                    if(message.content === "?pingdominic"){
-                        message.channel.send("<@287696585142304769> you have been pinged by someone for some reason, probably because your bot is broken")
-                    } else if {
-                        if(message.content === "?pingemortal") {
-                            message.channel.send("<@261498586611712000> you have been pinged for some reason by someone")
-                        } else if{
-                            if(message.content === "?info"){
-                                const newembed = new Discord.MessageEmbed()
-                                .setTitle("**Info**")
-                                .setDescription("**This Bot is created with discord.js**")
-                                .addField("Invite Link","https://discordapp.com/oauth2/authorize?client_id=389240402261311498&scope=bot&permissions=8")
-                                .addField("Support", "https://discord.gg/KJRM2Zv or message **DominicCM#2360** on discord ")
-                                .setFooter("Made by DominicCM, NathIsEmortal, Deni")
-                                .setTimestamp()
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    }
+    if(message.content === "?help"){
+        const embed = new Discord.MessageEmbed()
+        .setTitle("**Help Menu**")
+        .setDescription("**The bots prefix is: ?***")
+        .addField("Commands", "`ping` `whoiscool` `whatareyou` `pingdominic` `pingmortal` `pingpotato` `banurself` `kickurself` `kick` `ban` `info`")
+        .setFooter("Made by DominicCM, NathIsEmortal, Deni")
+        .setTimestamp()
+        message.channel.send(embed)
+    }
+    if(message.content === "?whoiscoool"){
+        message.channel.send("Is cool (JK emortal is)")
+    }
+    if(message.content === "?whatareyou"){
+        message.channel.send("I am a bot. https://www.quora.com/What-is-a-discord-bot-What-is-a-discord-server")
+    } 
+    if(message.content === "?pingdominic"){
+        message.channel.send("<@287696585142304769> you have been pinged by someone for some reason, probably because your bot is broken")
+    }
+    if(message.content === "?pingemortal") {
+        message.channel.send("<@261498586611712000> you have been pinged for some reason by someone")
+    } 
+    if(message.content === "?info"){
+         const newembed = new Discord.MessageEmbed()
+         .setTitle("**Info**")
+         .setDescription("**This Bot is created with discord.js**")
+         .addField("Invite Link","https://discordapp.com/oauth2/authorize?client_id=389240402261311498&scope=bot&permissions=8")
+         .addField("Support", "https://discord.gg/KJRM2Zv or message **DominicCM#2360** on discord ")
+         .setFooter("Made by DominicCM, NathIsEmortal, Deni")
+         .setTimestamp()
     }
 });
 
